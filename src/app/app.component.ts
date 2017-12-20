11cf0d4fedeb31496bb6ce0828c52a89531d081f
kirio93 <manuel.ferrucci93@gmail.com>
2017-12-20 17:59:24 +0100

    delete menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,9 +37,9 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  //openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+ //   this.nav.setRoot(page.component);
+ // }
 }
